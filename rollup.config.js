f6bba556b8ced9d7b35304b6345e32dfcf158cef
Mauricio Allende <mauricio.allende.o@gmail.com>
2018-06-11 17:45:04 -0400

    Add external dep support to rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,7 @@
 import eslint from 'rollup-plugin-eslint'
+import commonjs from 'rollup-plugin-commonjs'
+import nodeResolve from 'rollup-plugin-node-resolve'
+import cleanup from 'rollup-plugin-cleanup'
 
 export default {
   input: 'src/contract.js',
@@ -6,5 +9,16 @@ export default {
     file: 'dist/contract.js',
     format: 'cjs'
   },
-  plugins: [eslint()]
+  plugins: [
+    eslint(),
+    nodeResolve({
+      jsnext: true,
+      main: true
+    }),
+    commonjs({
+      include: 'node_modules/**',
+      extensions: ['.js']
+    }),
+    cleanup()
+  ]
 }
